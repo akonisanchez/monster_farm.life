b2c409481fc5389af0b1cecb79c9c367d52ad175
akonisanchez <sanchezakoni@gmail.com>
2025-01-13 20:56:55 -0800

    fixes...

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,34 +1,28 @@
 # This is the main controller for our game
 # It handles all the game actions like training and resting
 class GameController < ApplicationController
-    # Get our monster before doing anything
     before_action :get_monster
   
-    # Show the main game page
     def show
-      # If monster is too tired, show game over screen
       render :game_over if @monster.tiredness >= 10
     end
   
-    # This handles what happens when we click a training button
     def train
       success = @monster.train(params[:drill])
       flash[:notice] = if success
-                        "Training successful!"
-                      else
-                        "Training failed..."
-                      end
+        "Training successful!"
+      else
+        "Training failed..."
+      end
       redirect_to game_path
     end
   
-    # This handles what happens when we click the rest button
     def rest
       @monster.rest
       flash[:notice] = "Scrappo took a rest"
       redirect_to game_path
     end
   
-    # This resets the game when we want to start over
     def reset
       @monster.update(
         power: 1,
@@ -42,7 +36,6 @@ class GameController < ApplicationController
   
     private
   
-    # This gets or creates our monster
     def get_monster
       @monster = Monster.find_or_create_by(name: "Scrappo") do |monster|
         monster.power = 1
