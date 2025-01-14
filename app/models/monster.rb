25c6349ebf0f06ebb9e5d056236e16691659b10e
akonisanchez <sanchezakoni@gmail.com>
2025-01-13 21:04:44 -0800

    Fixed ruby style issues and added Rubocop config -Added .rubocop.yml config -Fixed code style in controllers and models -Improved code documentation

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,42 +1,42 @@
 class Monster < ApplicationRecord
     MAX_STAT = 999
     MAX_TIREDNESS = 10
-  
+
     validates :name, presence: true
     validates :power, :speed, :defense, :health,
       numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: MAX_STAT }
     validates :tiredness,
       numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: MAX_TIREDNESS }
-  
+
     def train(drill_type)
       return false if tiredness >= MAX_TIREDNESS
-  
+
       success = rand(100) < 85
-  
+
       if success
         case drill_type
         when "sled_pull"
-          self.power = [power + rand(1..5), MAX_STAT].min
+          self.power = [ power + rand(1..5), MAX_STAT ].min
         when "parry"
-          self.defense = [defense + rand(1..5), MAX_STAT].min
+          self.defense = [ defense + rand(1..5), MAX_STAT ].min
         when "meditate"
-          self.health = [health + rand(1..7), MAX_STAT].min
+          self.health = [ health + rand(1..7), MAX_STAT ].min
         when "dash"
-          self.speed = [speed + rand(1..5), MAX_STAT].min
+          self.speed = [ speed + rand(1..5), MAX_STAT ].min
         end
         self.tiredness += 1
       else
         self.tiredness += 2
       end
-  
+
       save
       success
     end
-  
+
     def rest
       return false if tiredness >= MAX_TIREDNESS
       recovery = rand(1..2)
-      self.tiredness = [0, tiredness - recovery].max
+      self.tiredness = [ 0, tiredness - recovery ].max
       save
     end
-  end
+end
