2bcad5c9012728dab42663bef91c664730d2a643
akonisanchez <sanchezakoni@gmail.com>
2025-01-13 20:53:51 -0800

    Basic game functionality fixing errors: -Added routes for game actions -Created game controller with training and rest ability -Added basic game views with stats and buttons -Fixed Ruby style issues

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@
 # Think of it like a map that tells visitors where to go
 Rails.application.routes.draw do
   # When someone visits the homepage ('/'), show them the game
-  root 'game#show'
-  
+  root "game#show"
+
   # Set up addresses for different game actions
-  get '/game', to: 'game#show', as: :game    # Show the game screen
-  post '/train', to: 'game#train', as: :train # Handle training actions
-  post '/rest', to: 'game#rest', as: :rest    # Handle resting
-  post '/reset', to: 'game#reset', as: :reset # Reset the game
+  get "/game", to: "game#show", as: :game    # Show the game screen
+  post "/train", to: "game#train", as: :train # Handle training actions
+  post "/rest", to: "game#rest", as: :rest    # Handle resting
+  post "/reset", to: "game#reset", as: :reset # Reset the game
 end
