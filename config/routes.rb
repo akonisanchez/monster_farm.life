c374dcc9e4d2a70bb579ffedb87338ecf17e231c
akonisanchez <sanchezakoni@gmail.com>
2025-01-16 12:45:12 -0800

    Add auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  
+  # Authentication routes
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  # Game routes
   get "/game", to: "game#show", as: :game
   post "/train", to: "game#train", as: :train
   post "/rest", to: "game#rest", as: :rest
