235f4c09a75e06c571d869b3a93fb0833c4677b7
akonisanchez <sanchezakoni@gmail.com>
2025-01-18 15:57:01 -0800

    Add monster selection controlller logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,45 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to game_path, notice: "Welcome to MonsterFarm.Life!"
+      redirect_to monster_selection_path, notice: "Welcome to MonsterFarm.Life! Choose your monster."
     else
       render :new
     end
+  end
+
+  def choose_monster
+    type = params[:monster_type]
+    case type
+    when 'chocobat'
+      monster = current_user.create_monster(
+        name: "Chocobat",
+        power: 5,
+        speed: 5,
+        defense: 1,
+        health: 1,
+        tiredness: 0
+      )
+    when 'flopower'
+      monster = current_user.create_monster(
+        name: "Flopower",
+        power: 5,
+        speed: 1,
+        defense: 5,
+        health: 1,
+        tiredness: 0
+      )
+    when 'galoot'
+      monster = current_user.create_monster(
+        name: "Galoot",
+        power: 3,
+        speed: 3,
+        defense: 3,
+        health: 3,
+        tiredness: 0
+      )
+    end
+    
+    redirect_to game_path
   end
 
   private
