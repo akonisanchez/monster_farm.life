07ee61e6d285b5e69a7d437115ab025884a2b684
akonisanchez <sanchezakoni@gmail.com>
2025-01-19 10:20:37 -0800

    Fix linting issues: removing trailing whites[pace and add final newlines

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
-  
-  validates :username, presence: true, 
-            uniqueness: { message: "is already taken" }, 
-            length: { minimum: 3, maximum: 20, 
+
+  validates :username, presence: true,
+            uniqueness: { message: "is already taken" },
+            length: { minimum: 3, maximum: 20,
                      message: "must be between 3 and 20 characters" }
-  validates :password, length: { minimum: 6, 
-                     message: "must be at least 6 characters long" }, 
+  validates :password, length: { minimum: 6,
+                     message: "must be at least 6 characters long" },
             on: :create
 
   has_one :monster, dependent: :destroy
